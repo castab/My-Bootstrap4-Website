a05ae4aa741598c57f1ed632f035a6ed2d22fe30
brayan <32408642+castab@users.noreply.github.com>
2018-11-22 14:05:02 -0800

    Feature adds
    I think I'm gonna swap over to Bootstrap

--- app.js
+++ app.js
@@ -20,9 +20,15 @@
             vm.introComplete = false;
         }
 
-        $http.get('navigate.json').then(
+        vm.changePage = function(project) {
+            vm.currentProject = project;
+            $location.path(project.route);
+        }
+
+        $http.get('data.json').then(
             function(resp) {
                 vm.projectData = resp.data.projects;
+                vm.currentProject = vm.projectData.current[0];
             },
             function (error) {
                 console.log("error getting data")
@@ -31,8 +37,10 @@
 
 
     })
-    .controller('navbarCtrl', function($location) {
-        vm = this;
+    .controller('navbarCtrl', function($scope, $location, $http) {
+        var vm = {};
+        $scope.vm = vm;
+
         vm.isNavbarActive = function() {
             if ($location.path() != '/') {
                 return true;
@@ -40,6 +48,14 @@
                 return false;
             }
         }
+        $http.get('data.json').then(
+            function(resp) {
+                vm.projectData = resp.data.projects;
+            },
+            function (error) {
+                console.log("error getting data")
+            }
+        )
     })
     .run(function($http, $templateCache) {
         $http.get('menu.html').then(
@@ -49,7 +65,7 @@
             function(error) {
                 console.log('Error caching menu.html');
             }
-        )
+        );
     })
     .animation('.jqslide', [function() {
         return {
@@ -68,23 +84,6 @@
             }
         }
     }])
-    .animation('.menuBurger', [function() {
-        return {
-            enter: function(element, doneFn) {
-                element.css('display', 'none');
-                jQuery(element).slideDown('fast', doneFn);
-                return function(isCancelled) {
-                    element.stop();
-                }
-            },
-            leave: function(element, doneFn) {
-                jQuery(element).slideUp('fast', doneFn);
-                return function(isCancelled) {
-                    element.stop();
-                }
-            }
-        }
-    }])
     .config(function($routeProvider) {
         $routeProvider
         .when("/", {
@@ -92,6 +91,27 @@
         })
         .when("/menu", {
             templateUrl: "menu.html"
+        })
+        .when("/rgbleds", {
+            templateUrl: "project.html"
+        })
+        .when("/webdev", {
+            templateUrl: "project.html"
+        })
+        .when("/iot", {
+            templateUrl: "project.html"
+        })
+        .when("/clmc", {
+            templateUrl: "project.html"
+        })
+        .when("/slam", {
+            templateUrl: "project.html"
+        })
+        .when("/wildwest", {
+            templateUrl: "project.html"
+        })
+        .when("/bluetooth", {
+            templateUrl: "project.html"
         })
     })
 
