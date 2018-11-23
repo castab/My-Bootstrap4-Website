c9f0b0586ba70a85b2659e8e13071a9b8d7861cc
brayan <32408642+castab@users.noreply.github.com>
2018-11-23 15:12:48 -0800

    Optimization and Cleanup
    - Cleaned up the HTML, more emphasis on letting AngularJS do the heavy lifting
    - Cleaned up code so that minification works
    - Starting to use ng-bind instead of {{ }} method
    - Changed data.json contract and adjusted the project to accomodate

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function () {
     angular.module('app', ['ngAnimate', 'ngRoute'])
-    .controller('mainCtrl', function($scope, $http, $timeout, $location) {
+    .controller('mainCtrl', ['$location', '$rootScope', 'projectDataService', function($location, $rootScope, projectDataService) {
 
         vm = this;
         vm.exists = true;
@@ -9,8 +9,8 @@
         vm.introComplete = false;
 
         vm.click = function() {
-            vm.exists = !vm.exists;
-            vm.buttonClicked = !vm.buttonClicked;
+            vm.exists = false;
+            vm.buttonClicked = true;
         }
         vm.clickStart = function() {
             vm.introComplete = true;
@@ -21,23 +21,19 @@
         }
 
         vm.changePage = function(project) {
-            vm.currentProject = project;
-            $location.path(project.route);
+            projectDataService.changePage(project);
+            vm.currentProject = projectDataService.getCurrentProject();
         }
 
-        $http.get('data.json').then(
-            function(resp) {
-                vm.projectData = resp.data.projects;
-                vm.currentProject = vm.projectData.current[0];
-            },
-            function (error) {
-                console.log("error getting data")
-            }
-        )
+        vm.projectData = projectDataService.getProjectData();
 
+        // Future use - matching up route with content being viewed
+        /* $rootScope.$on('$routeChangeSuccess', function() {
+            console.log('Route change detected.');
+        }); */
 
-    })
-    .controller('navbarCtrl', function($scope, $location, $http) {
+    }])
+    .controller('navbarCtrl', ['$scope', '$location', '$http', 'projectDataService', function($scope, $location, $http, projectDataService) {
         var vm = {};
         $scope.vm = vm;
 
@@ -47,17 +43,57 @@
             } else {
                 return false;
             }
-        }
+        };
+        vm.changePage = function(project) {
+            projectDataService.changePage(project);
+        };
+        
         $http.get('data.json').then(
             function(resp) {
                 vm.projectData = resp.data.projects;
             },
             function (error) {
-                console.log("error getting data")
+                console.log("error getting data");
             }
-        )
-    })
-    .run(function($http, $templateCache) {
+        );
+
+    }])
+    .service('projectDataService', ['$http', '$location', '$location', function($http, $location, $location) {
+        vm = this;
+        $http.get('data.json').then(
+            function(resp) {
+                vm.projectData = resp.data.projects;
+                vm.currentProject = vm.projectData.current.projects[0];
+            },
+            function (error) {
+                console.log("error getting data");
+            }
+        );
+        vm.changePage = function(project) {
+            vm.currentProject = project;
+            $('.navbar-collapse').collapse('hide');
+            $location.path(project.route);
+            window.scrollTo(0, 0);
+        };
+        vm.getCurrentProject = function() {
+            if (vm.currentProject) {
+                return vm.currentProject;
+            };
+        };
+        vm.getProjectData = function() {
+            if (vm.projectData) {
+                return vm.projectData
+            };
+        };
+        vm.isNavbarActive = function() {
+            if ($location.path() != '/') {
+                return true;
+            } else {
+                return false;
+            }
+        };
+    }])
+    .run(['$http', '$templateCache', function($http, $templateCache) {
         $http.get('menu.html').then(
             function(resp) {
                 $templateCache.put('menu.html', resp.data);
@@ -66,7 +102,7 @@
                 console.log('Error caching menu.html');
             }
         );
-    })
+    }])
     .animation('.jqslide', [function() {
         return {
             enter: function(element, doneFn) {
@@ -84,7 +120,7 @@
             }
         }
     }])
-    .config(function($routeProvider) {
+    .config(['$routeProvider', function($routeProvider) {
         $routeProvider
         .when("/", {
             templateUrl: "blank.html"
@@ -113,6 +149,9 @@
         .when("/bluetooth", {
             templateUrl: "project.html"
         })
-    })
+        .otherwise( {
+            redirectTo: "/menu"
+        })
+    }])
 
 })();
