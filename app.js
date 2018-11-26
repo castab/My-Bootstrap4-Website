41afc7027595c5f44dbe00d00e955ba3ea430f70
brayan <32408642+castab@users.noreply.github.com>
2018-11-26 09:50:25 -0800

    Updates and fixes
    - Some styling changes
    - Fixed the routes issue where a user would type in a routeable URL and view wouldn't update; good now
    - Added content
    - Fixed YouTube video container by creating a filter "trustUrl"
    - Getting the project data from the projectDataService now returns a promise object for better handling and caching

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
         vm = this;
         vm.exists = true;
         vm.buttonClicked = false;
-        vm.projectData = null;
+        vm.projectData = {};
         vm.introComplete = false;
 
         vm.click = function() {
@@ -22,20 +22,46 @@
 
         vm.changePage = function(project) {
             projectDataService.changePage(project);
-            vm.currentProject = projectDataService.getCurrentProject();
+            vm.currentProject = project;
         }
 
-        vm.projectData = projectDataService.getProjectData();
+        projectDataService.getProjectData()
+        .then(function(resp) {
+            vm.projectData = resp;
+        }, function(error) {
+            console.log('There was an error fetching the data.');
+        });
 
-        // Future use - matching up route with content being viewed
-        /* $rootScope.$on('$routeChangeSuccess', function() {
-            console.log('Route change detected.');
-        }); */
+        $rootScope.$on('$routeChangeSuccess', function() {
+            var slash = '/';
+            // This doesn't need to run on the /menu or / pages
+            if (!($location.path() == '/menu' || $location.path() == '/')) {
+                // Need to evaluate what project should be loaded
+                // Check if vm.projectData has content
+                if (vm.projectData.current) {
+                    // Looks like content is loaded
+                    // Check if current project and route match up
+                    if (!(slash.concat(vm.currentProject.route) == $location.path())) {
+                        // Mismatch, get correct current project and load it
+                        _.forEach(vm.projectData, function(currentOrPreviousProjects) {
+                            _.forEach(currentOrPreviousProjects.projects, function(project) {
+                                if (slash.concat(project.route) == $location.path()) {
+                                    vm.changePage(project);
+                                    // console.log('View corrected.')
+                                    return false;
+                                }
+                            })
+                        })
+                    }
+                }
+            }
+        });
 
     }])
-    .controller('navbarCtrl', ['$scope', '$location', '$http', 'projectDataService', function($scope, $location, $http, projectDataService) {
+    .controller('navbarCtrl', ['$scope', '$location', 'projectDataService', function($scope, $location, projectDataService) {
         var vm = {};
         $scope.vm = vm;
+        vm.projectData = {};
 
         vm.isNavbarActive = function() {
             if ($location.path() != '/') {
@@ -48,27 +74,18 @@
             projectDataService.changePage(project);
         };
         
-        $http.get('data.json').then(
-            function(resp) {
-                vm.projectData = resp.data.projects;
-            },
-            function (error) {
-                console.log("error getting data");
-            }
-        );
+        projectDataService.getProjectData()
+        .then(function(resp) {
+            vm.projectData = resp;
+        }, function(error) {
+            console.log("Failed to load Navbar project data")
+        })
 
     }])
-    .service('projectDataService', ['$http', '$location', '$location', function($http, $location, $location) {
+    .service('projectDataService', ['$http', '$location', '$q', function($http, $location, $q) {
         vm = this;
-        $http.get('data.json').then(
-            function(resp) {
-                vm.projectData = resp.data.projects;
-                vm.currentProject = vm.projectData.current.projects[0];
-            },
-            function (error) {
-                console.log("error getting data");
-            }
-        );
+        vm.projectData = {};
+
         vm.changePage = function(project) {
             vm.currentProject = project;
             $('.navbar-collapse').collapse('hide');
@@ -81,9 +98,28 @@
             };
         };
         vm.getProjectData = function() {
-            if (vm.projectData) {
-                return vm.projectData
-            };
+            var defer = $q.defer(); // Start up a promise object
+            if (vm.projectData.current) {
+                // Data has already been loaded, resolve the promise with payload
+                defer.resolve(vm.projectData);
+            } else {
+                // Data has not been loaded, fetch it
+                $http.get('data.json').then(
+                    function(resp) {
+                        vm.projectData = resp.data.projects;
+                        vm.currentProject = vm.projectData.current.projects[0];
+                        // All set, resolve the promise with payload
+                        defer.resolve(vm.projectData);
+                    },
+                    function (error) {
+                        console.log("error getting data");
+                        // Can't get it for some reason, reject with error
+                        defer.reject(error);
+                    }
+                );
+            }
+            // Return the promise object
+            return defer.promise;
         };
         vm.isNavbarActive = function() {
             if ($location.path() != '/') {
@@ -120,10 +156,15 @@
             }
         }
     }])
+    .filter('trustUrl', function($sce) {
+        return function(url) {
+            return $sce.trustAsResourceUrl(url);
+        }
+    })
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
         .when("/", {
-            templateUrl: "blank.html"
+            templateUrl: "intro.html"
         })
         .when("/menu", {
             templateUrl: "menu.html"
